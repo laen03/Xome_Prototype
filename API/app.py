8534de9b78083a10c9c0ee1761b59f89a5f37ad4
unknown <leandro.vasvega@gmail.com>
2023-06-23 08:20:20 -0600

    Update app.py
    Added a dictionary to choice randomly on of them and use it in the prompt, also implemented the price feature. Now the prompt changes according to the price and number of rooms of each property.
    
    Co-Authored-By: IsaacG9 <122411364+IsaacG9@users.noreply.github.com>

--- API/app.py
+++ API/app.py
@@ -8,6 +8,7 @@ from flask_cors import CORS
 from flask import url_for
 from flask import request
 from PIL import Image
+import random
 
 
 openai.api_key = "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"
@@ -96,8 +97,18 @@ def readJson():
     # summer day in Jackson city, extreme long-shot on iPhone 6
     
     for property in properties:
+        
+        usePrompt1 = random.choice([False, True])
         prompt = "A charming "
         
+        places = {
+            "Utah":"Salt Lake", 
+            "Florida":"Miami", 
+            "New York": "Brooklyn", 
+            "Pennsylvania": "Philadelphia"
+        }
+        state, district = random.choice(list(places.items()))
+        prompt2 = "Create a color photography taken from the street in " + state + " of "
         # Number of rooms
         rooms = str(len(property["rooms"]))
         prompt += rooms + "-room, "
@@ -109,35 +120,89 @@ def readJson():
         
         # Property value
         propertyValue = property["expectedValue"]
+
+
         # barata 0-250 000
         # medio: 250 001 - 700 000
         # alto: 700 001 - en adelante
         
-        if propertyValue <= 250000:
-            pass
-        elif propertyValue > 250000 and propertyValue <= 700000:
-            if int(rooms) > 10:
-                # casa baja
-                pass
-            elif int(rooms) <= 10:
-                # casa alta
-                pass
-        else:
-            # lo más caro papuuuu
-            pass
+        
         propertyType = property["propertyType"]
-        if propertyType == "SINGLE":
-            prompt += "single-family house, featuring a welcoming porch with steps leading up and a side porch. The image is taken from the street at midday."
-        elif propertyType == "DUPLEX":
-            prompt += "duplex house, featuring a welcoming porch with steps leading up and a side porch. The image is taken from the street at midday."
-        elif propertyType == "TOWNHOUSE":
-            prompt += "brick townhouse, nestled in a tree-lined street, with unique architectural details that add character and charm. The sun is shining and the photo is taken from the street."
-        elif propertyType == "MOBILE":
-            continue
+        if propertyType == "MOBILE":
+            weather = ["a summer", "a winter", "a cloudy", "an overcast"]
+            prompt = prompt2 = "A cozy, weathered mobile home nestled in a nice neighborhood surrounded by other mobile houses, with a clear blue sky overhead. The photo was taken from the street in " + random.choice(weather) + " day"
+        else:
+            # "Cheap" properties
+            if propertyValue <= 250000:
+                if propertyType == "SINGLE":
+                    prompt2 += "an old, one-story house built in 1971, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                    prompt = prompt2
+                elif propertyType == "DUPLEX":
+                    prompt2 += "an old, two-story house built in 1971, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                    prompt = prompt2
+                elif propertyType == "TOWNHOUSE":
+                    prompt2 += "a townhouse, with cheap facade. The scene is rich with detail and character, featuring lush green trees. The image captures the essence of urban low class architecture in this iconic " + state + ". The prompt's careful attention to detail, composition, and mood ensures that the DALL-E model can produce stunning and high-quality images of one of " + district + "'s most iconic architectural styles."
+                    prompt = prompt2
+            
+            # "Mid-range" properties
+            elif propertyValue > 250000 and propertyValue <= 700000:
+                # "Cheap" properties
+                if int(rooms) > 10:
+                    if propertyType == "SINGLE":
+                        prompt2 += "an old, one-story house built in 1971, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                        prompt = prompt2
+                    elif propertyType == "DUPLEX":
+                        prompt2 += "an old, two-story house built in 1971, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                        prompt = prompt2
+                    elif propertyType == "TOWNHOUSE":
+                        prompt2 += "a townhouse, with cheap facade. The scene is rich with detail and character, featuring lush green trees. The image captures the essence of urban low class architecture in this iconic " + state + ". The prompt's careful attention to detail, composition, and mood ensures that the DALL-E model can produce stunning and high-quality images of one of " + district + "'s most iconic architectural styles."
+                        prompt = prompt2
+
+                # "Expensive" properties
+                elif int(rooms) <= 10:
+                    if propertyType == "SINGLE":
+                        prompt += "single-family house, featuring a welcoming porch with steps leading up and a side porch. The image is taken from the street at midday."
+                    
+                        prompt2 += "an new, one-story fancy house built in 2000, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                    elif propertyType == "DUPLEX":
+                        prompt += "duplex house, featuring a welcoming porch with steps leading up and a side porch. The image is taken from the street at midday."
+                        
+                        prompt2 += "an new, two-story fancy house built in 2000, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                    elif propertyType == "TOWNHOUSE":
+                        prompt += "brick townhouse, nestled in a tree-lined street, with unique architectural details that add character and charm. The sun is shining and the photo is taken from the street."
+                        
+                        prompt2 += "a townhouse, with elegant facade. The scene is rich with detail and character, featuring lush green trees. The image captures the essence of urban architecture in this iconic " + state + ". The prompt's careful attention to detail, composition, and mood ensures that the DALL-E model can produce stunning and high-quality images of one of " + district + "'s most iconic architectural styles."
+
+            # "Expensive" properties
+            else:
+                if propertyType == "SINGLE":
+                    prompt += "single-family house, featuring a welcoming porch with steps leading up and a side porch. The image is taken from the street at midday."
+                    
+                    prompt2 += "an new, one-story fancy house built in 2000, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                elif propertyType == "DUPLEX":
+                    prompt += "duplex house, featuring a welcoming porch with steps leading up and a side porch. The image is taken from the street at midday."
+                    
+                    prompt2 += "an new, two-story fancy house built in 2000, nestled in a peaceful suburban neighborhood, surrounded by neat and tidy greenery."
+                elif propertyType == "TOWNHOUSE":
+                    prompt += "brick townhouse, nestled in a tree-lined street, with unique architectural details that add character and charm. The sun is shining and the photo is taken from the street."
+                    
+                    prompt2 += "a townhouse, with elegant facade. The scene is rich with detail and character, featuring lush green trees. The image captures the essence of urban architecture in this iconic " + state + ". The prompt's careful attention to detail, composition, and mood ensures that the DALL-E model can produce stunning and high-quality images of one of " + district + "'s most iconic architectural styles."
+        
+        if usePrompt1:
+            if  propertyType != "TOWNHOUSE":
+                finalPrompt = prompt
+            else:
+                finalPrompt = prompt2
+            
+        else:
+            finalPrompt = prompt2
+        
+            
+                    
         try:
             response = openai.Image.create(
-                prompt = prompt,
-                n = 1,
+                prompt = finalPrompt,
+                n = 3,
                 size ="512x512"
             )
             
@@ -152,13 +217,14 @@ def readJson():
                     file_path = "images/" + file_name + ".jpeg"
 
                     with open(file_path, "wb") as f:
-                        image.save(f, "JPEG", quality = 20, optimize=True)
+                        image.save(f, "JPEG", quality = 35, optimize=True)
 
                     print("Success")
                 except Exception as e:
                     print('FAILED -', e)
         except openai.error.InvalidRequestError as e:
             return str(e)
+        print(finalPrompt)
 
     # Closing file
     f.close()
